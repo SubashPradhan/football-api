850ebb3e1b49cd802698565001d4a6fde4e008cc
Subash Pradahan <suprowilde5@gmail.com>
2019-07-31 13:27:04 +0200

    Remove node_modules

---
+++ db.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const databaseUrl = 'postgres://postgres:secret@localhost:5432/football-api'
+const db = new Sequelize(databaseUrl)
+
+db.sync()
+  .then(() => console.log("Database has been updated!"))
+  .catch((error) => {
+    console.log(error)
+  }) 
+
+module.exports = db  
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const db = require('./db')
+const model = require('./team/model')
 const port = 4000
 
 app.get('/', (require, response) => response.send('Welcome'))
